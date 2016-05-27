91d5f7136c1768b555c26f327fc046728430db04
Alan Edwardes <alan@alanedwardes.com>
2016-05-27 19:03:23 +0100

    just analyse with imagemagick

--- entry.py
+++ entry.py
@@ -1,13 +1,26 @@
+import subprocess
 import boto3
 import urllib
 
 s3 = boto3.resource('s3')
+
+IMAGEMAGICK_IDENTIFY = '/usr/bin/identify'
+DOWNLOAD_PATH = '/tmp/image.jpg'
 
 def handler(event, context):
   bucket_name = urllib.unquote(event['Records'][0]['s3']['bucket']['name'])
   key_name = urllib.unquote(event['Records'][0]['s3']['object']['key'])
 
   bucket = s3.Bucket(bucket_name)
-  object = bucket.Object(key_name).get()
+  bucket.download_file(key_name, DOWNLOAD_PATH)
 
-  return object['Body']
+  params = [
+    IMAGEMAGICK_IDENTIFY,
+    DOWNLOAD_PATH
+  ]
+  
+  print('Invoking ' + ' '.join(params))
+  process = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  
+  print('ffmpeg stdout: ' + process.stdout.read())
+  print('ffmpeg stderr: ' + process.stderr.read())

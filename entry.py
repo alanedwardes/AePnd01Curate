ec777d3b04fb111b8885ea2671f65d9c3d272bc6
Alan Edwardes <alan@alanedwardes.com>
2016-05-27 18:45:47 +0100

    unquote event input

--- entry.py
+++ entry.py
@@ -1,9 +1,13 @@
 import boto3
+import urllib
 
 s3 = boto3.resource('s3')
 
 def handler(event, context):
-  bucket = s3.Bucket(event['Records'][0]['s3']['bucket']['name'])
-  object = bucket.Object(event['Records'][0]['s3']['object']['key']).get()
+  key_name = urllib.unquote(event['Records'][0]['s3']['object']['key'])
+  bucket_name = urllib.unquote(event['Records'][0]['s3']['bucket']['name'])
+
+  bucket = s3.Bucket(bucket_name)
+  object = bucket.Object(key_name).get()
 
   return object['Body']

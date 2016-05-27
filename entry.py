274409ec05997e42695f20f17a6f5aed6ff807f5
Alan Edwardes <alan@alanedwardes.com>
2016-05-27 18:50:06 +0100

    Adds pillow lib

--- entry.py
+++ entry.py
@@ -4,8 +4,8 @@ import urllib
 s3 = boto3.resource('s3')
 
 def handler(event, context):
-  key_name = urllib.unquote(event['Records'][0]['s3']['object']['key'])
   bucket_name = urllib.unquote(event['Records'][0]['s3']['bucket']['name'])
+  key_name = urllib.unquote(event['Records'][0]['s3']['object']['key'])
 
   bucket = s3.Bucket(bucket_name)
   object = bucket.Object(key_name).get()

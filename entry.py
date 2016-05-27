246f3ae9c81bc64a6ded216fd63aac5be192ef5c
Alan Edwardes <alan@alanedwardes.com>
2016-05-27 18:41:46 +0100

    Actually get the object

--- entry.py
+++ entry.py
@@ -3,7 +3,7 @@ import boto3
 s3 = boto3.resource('s3')
 
 def handler(event, context):
-  bucket = event['Records'][0]['s3']['bucket']
-  object = event['Records'][0]['s3']['object']
+  bucket = s3.Bucket(event['Records'][0]['s3']['bucket']['name'])
+  object = bucket.Object(event['Records'][0]['s3']['object']['key'])
 
-  return object['key']
+  return object.key
